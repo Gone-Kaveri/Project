b5d5bb39abb4398b11a6b014170263f235670538
Nagarapu Geetha <ngeetha2003@gmail.com>
2022-11-13 14:39:00 +0530

    changes done

--- src/App.js
+++ src/App.js
@@ -1,27 +1,59 @@
-import React from 'react'
 import './App.css';
-import Navbar from './containers/navpage';  //exported from navpage
+import Navbar from './containers/navbar';  //exported from navpage
 import "bootstrap/dist/css/bootstrap.min.css"
 import "./App.css"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Login from "./containers/login"
 import SignUp from "./containers/signup"
 import Profile from './containers/profile';
-import HomePage from './containers/homepage';
-import NavPage from "./containers/navpage"
+import Home from './containers/homepage';
+import MyOrders from './containers/myorder';
+
+//db url
+import nba from "nba/dist/nba"
+const DBUrl="mongodb+srv://onlinestore.xgz61ga.mongodb.net/store"
+const userName="onlineStoreDB"
+const password="onlineStoreDB123"
+//mongodb
+const exp=require("express")
+const app=exp()
+
+const mclient=require("mongodb").MongoClient;
+//connect with mongoDB server
+mclient.connect(DBUrl) 
+.then((client)=>{
+  // get DB object
+  let dbobj=client.db("store")
+
+  //create collection objects
+  let customersCollectionObj=dbobj.collection("customers");
+  let ordersCollectionObj=dbobj.collection("customersOrders");
+
+  //sharing collection objects to APIS
+  app.set("customersCollectionobj",customersCollectionObj);
+  app.set("ordersCollectionObj",ordersCollectionObj);
+
+  console.log("DB connection successful")
+})
+.catch(err=>console.log("error in db connection:",err));
+
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path='/' exact component={HomePage} />
-        <Route path='/login' component={Login} />
-        <Route path='/profile' component={Profile} />
-        <Route path='/signup' component={SignUp} />
+     <BrowserRouter>
+      <div>
+     <Navbar/>
+     
+       <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/profile' element={<Profile/>} />
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/signup' element={<SignUp/>}/>
+        <Route path='/myorders' element={<MyOrders/>}/>
       </Routes>
-    </BrowserRouter>
+      </div>
+     </BrowserRouter>
+    
   );
 }
 
-export default App
-
+export default App;
---
+++ src/containers/navbar.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Router } from "react-router-dom";
+import { Link } from "react-router-dom";
+// import { Navbar, Nav } from 'rsuite';
+const Navbar = () => {
+    return (
+            <div style={{display:"flex",justifyContent:"space-around",background:"dodgerblue",height:"50px",fontSize:"large",alignContent:"center", color:"black",}}>
+                    <div><Link to="/" style={{color:"black",textDecoration:"none"}}>Home</Link></div>
+                    <div><Link to="/profile" style={{color:"black",textDecoration:"none"}}>Profile</Link></div> 
+                    <div><Link to="/myorders" style={{color:"black",textDecoration:"none"}}>My Orders</Link></div>
+                    <div><Link to="/login" style={{color:"black",textDecoration:"none"}}>Login</Link></div>
+                    <div><Link to="/signup" style={{color:"black",textDecoration:"none"}}>Signup</Link></div>
+            </div >
+    
+    );
+};
+
+export default Navbar;
